9b290a07b291d02b1dce4d62a1e8c4efcc1541e8
Gudrutis <mr.matonis@gmail.com>
2015-10-22 12:45:51 +0300

    beveik padarytas 2 punktas

---
+++ constants.h
@@ -0,0 +1,14 @@
+#include <windows.h>
+#ifndef CONSTANTS_H_INCLUDED
+#define CONSTANTS_H_INCLUDED
+/*drop dawn maniu*/
+#define IDR_MAIN_MENU             2000
+#define IDM_FILE_OPEN             2100
+#define IDM_FILE_EXIT             2105
+
+/* dialogams */
+#define IDC_STATIC 				-1
+#define IDD_ABOUT               102
+
+
+#endif // CONSTANTS_H_INCLUDED
---
+++ main.cpp
@@ -0,0 +1,83 @@
+#include <windows.h>
+#include "constants.h"
+
+
+/*coment comments more comments*/
+long __stdcall WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)                  /* handle the messages */
+    {
+        /* right click'inant issoka langas*/
+        case WM_RBUTTONDOWN:
+        {
+            char szFileName[MAX_PATH];
+            HINSTANCE hInstance = GetModuleHandle(NULL);
+            GetModuleFileName(hInstance, szFileName, MAX_PATH);
+            MessageBox(hwnd, szFileName, "This program is:", MB_YESNO/* | MB_ICONINFORMATION*/);
+        }
+        break;
+        case WM_DESTROY:
+            PostQuitMessage (0);       /* send a WM_QUIT to the message queue */
+            break;
+            /*? atskiras casas maniu punktui??? ?*/
+        case WM_COMMAND:
+            {
+            switch(LOWORD(wParam))
+                {
+
+                case IDM_FILE_EXIT:
+                    {
+                        PostQuitMessage (0);       /* send a WM_QUIT to the message queue */
+
+                    }
+                break;
+                case IDM_FILE_OPEN:
+                    {
+                    char szFileName[MAX_PATH];
+                    HINSTANCE hInstance = GetModuleHandle(NULL);
+                    GetModuleFileName(hInstance, szFileName, MAX_PATH);
+                    MessageBox(hwnd, szFileName, "This program is:", MB_OK |
+                        MB_ICONINFORMATION);
+                    }
+                break;
+                /*
+                case IDOK:
+					EndDialog(hwnd, IDOK);
+				break;
+				case IDCANCEL:
+					EndDialog(hwnd, IDCANCEL);
+				break;
+				*/
+                }
+            }
+        default:                      /* for messages that we don't deal with */
+            return DefWindowProc (hwnd, message, wParam, lParam);
+    }
+ return 0;
+}
+
+int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrev, LPSTR lpszArgument, int nCmdShow)
+{
+ char szClassName[ ] = "CodeBlocksWindowsApp";
+ MSG messages;
+ WNDCLASS wc;
+ HWND hwnd;
+
+ wc.hInstance     = hInstance,                         wc.lpszClassName  = szClassName;
+ wc.lpfnWndProc   = WindowProcedure,                   wc.style          = 0;
+ wc.hIcon         = LoadIcon(NULL, IDI_APPLICATION),   wc.hCursor        = LoadCursor(NULL, IDC_ARROW);
+ wc.lpszMenuName  = MAKEINTRESOURCE(IDR_MAIN_MENU),    wc.cbClsExtra     = 0;
+ wc.cbWndExtra    = 0,                                 wc.hbrBackground  = (HBRUSH)COLOR_BACKGROUND;
+ RegisterClass(&wc);
+ hwnd=CreateWindow(szClassName,"Template",WS_OVERLAPPEDWINDOW,50,50,544,375,HWND_DESKTOP,NULL,hInstance,NULL);
+ ShowWindow (hwnd, nCmdShow);
+ while(GetMessage (&messages, NULL, 0, 0)>0)
+ {
+    TranslateMessage(&messages);
+    DispatchMessage(&messages);
+ }
+
+ return messages.wParam;
+}
+
+
